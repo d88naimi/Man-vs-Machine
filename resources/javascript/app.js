41006f53ad6a057ced497958af44a94d9b615a78
William Hansen <williamhansen@Williams-MacBook-Pro.local>
2017-03-09 21:12:09 -0800

    end of class

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -6,29 +6,54 @@
 
 // https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple
 
+var questions = [];
+
 var wolfram = {
 	queryWolfram: function(queryString){
 		var queryObj = {
 			appid: "A6PYJY-7QJY4JG733",
-			i: queryString
+			input: queryString,
+			output: "json",
+			format: "plaintext"
 		};
-		var URL = "https://api.wolframalpha.com/v1/result?";
+		var URL = "http://api.wolframalpha.com/v2/query?";
+		// var URL = "http://api.wolframalpha.com/v1/result?";
 		var queryURL = URL + $.param(queryObj);
 		$.ajax({
     		url: queryURL,
-    		method: "GET"
+    		method: "GET",
+    		crossDomain: true,
+    		dataType: "jsonp"
   		}).done(function(response) {
-  			console.log(response);
+  			if (response.queryresult.success) {
+  				if (questions.length < 10) {
+  					console.log(queryString);
+  					questions.push(queryString);
+  					console.log(questions);
+  				}
+  			}
   		});
 	}
 };
 
 var trivia = {
-
+	queryTrivia: function(amount){
+		var queryURL = "https://opentdb.com/api.php?amount="+amount+"&category=22&type=multiple";
+		$.ajax({
+    		url: queryURL,
+    		method: "GET",
+  		}).done(function(response) {
+  			for (i=0; i<response.results.length;i++){
+  				// console.log(response.results[i].question);
+  				wolfram.queryWolfram(response.results[i].question);
+  			};
+  			console.log(response);
+  		});
+	}
 };
 
 var cleverBot = {
 
 };
 
-wolfram.queryWolfram("how far away is the moon?");
+trivia.queryTrivia(20);
