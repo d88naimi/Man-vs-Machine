082bba3f356434622e232573a6cf3d30b1c8baa3
Little, Andrew <Andrew_Little@intuit.com>
2017-03-14 21:32:58 -0700

    added images, and made html changes

---
+++ resources/javascript/front.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$('#wolframAnswered').hide();
+})
+
+
+$('#an1').click(function(){
+
+	var wolframAnswered = "Wolfram Answered!";
+
+	// $('#an2').removeClass('btn-primary').addClass('btn-danger').addClass('wrongAnswer');
+
+	$('#an3').removeClass('btn-info').addClass('btn-success').addClass('rightAnswer');
+
+	// $('#whoAnsweredThisMessage').animate({
+	// 	marginTop:0,
+	// 	duration: 800
+
+	// }, 500, function(){
+	// 	$(this).after("<div>" + wolframAnswered + "</div>");
+
+	// });
+
+});
+
+
+$('#an5').click(function(){
+
+	$('.disAp').fadeOut("slow");
+});
+
+$('#clickMeOne').on("click", function(){
+
+	$('#wolframAnswered').slideDown();
+
+})
